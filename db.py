5ce7a38b28859b12a25472a2c54cef4ce1275d62
Kogoon <gojs712@gmail.com>
2022-05-11 10:17:00 +0900

    Connect Sqlite3 & Create Table

--- db.py
+++ db.py
@@ -1,11 +1,10 @@
 import sqlite3
 
 conn = sqlite3.connect('database.db')
-print('Create & Connect database')
+print('Create & Connect Database')
 
-conn.execute(
-create table menus (menu text)
-)
+conn.execute('create table menus (id num, menu text)')
+print('Create Table')
 
 conn.close()
 
