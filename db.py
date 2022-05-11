7d6fba369dd054409b11034e2ff9723d2b1cf68a
Kogoon <gojs712@gmail.com>
2022-05-11 10:37:02 +0900

    Modified DB

--- db.py
+++ db.py
@@ -3,7 +3,8 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print('Create & Connect Database')
 
-conn.execute('create table menus (id num, menu text)')
+conn.execute('create table menus (id integer primary key autoincrement, \
+                                menu text)')
 print('Create Table')
 
 conn.close()
